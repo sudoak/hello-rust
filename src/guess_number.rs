be9aaff28748ed79c55797aa4f37321e6e737402
sudoak <sudoakk@gmail.com>
2020-04-23 15:40:45 +0530

    [fix] : continue program if non numeric types & break program if number equals

--- src/guess_number.rs
+++ src/guess_number.rs
@@ -11,11 +11,17 @@ pub fn guess_number(){
         stdin().read_line(&mut guessed_number)
             .expect("Failed to read number");
         
-        let guess: u32 = guessed_number.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guessed_number.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Small"),
             Ordering::Greater => println!("Big"),
-            Ordering::Equal => println!("Hurray its equal"),
+            Ordering::Equal => {
+                println!("Hurray its equal");
+                break;
+            }
         }
     }
 }
