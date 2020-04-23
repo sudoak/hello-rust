58485036341ab5ef34f7ff4d9318d797640d3336
sudoak <sudoakk@gmail.com>
2020-04-23 15:32:44 +0530

    [feat] : run program in loop

--- notest.md
+++ notest.md
@@ -1,4 +1,6 @@
 * cargo is the dependancy manager (check, run, build, --release, update)
 * for Result, the variants are Ok,Err
 * a match expression is made up of arms
+* use shadowing to overwrite existing values - especially for conversion
+* loop {} creates infinite loop
 * 
--- src/guess_number.rs
+++ src/guess_number.rs
@@ -4,16 +4,18 @@ use std::cmp::Ordering;
 
 pub fn guess_number(){
     let mut rng = thread_rng();
-    let secret_number = rng.gen_range(0, 10);
-    println!("Please guess a number");
-    let mut guessed_number = String::new();
-    stdin().read_line(&mut guessed_number)
-        .expect("Failed to read number");
-    
-    let guess: u32 = guessed_number.trim().parse().expect("Please type a number!");
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Small"),
-        Ordering::Greater => println!("Big"),
-        Ordering::Equal => println!("Hurray its equal"),
+    loop {
+        let secret_number = rng.gen_range(0, 10);
+        println!("Please guess a number");
+        let mut guessed_number = String::new();
+        stdin().read_line(&mut guessed_number)
+            .expect("Failed to read number");
+        
+        let guess: u32 = guessed_number.trim().parse().expect("Please type a number!");
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Small"),
+            Ordering::Greater => println!("Big"),
+            Ordering::Equal => println!("Hurray its equal"),
+        }
     }
 }
